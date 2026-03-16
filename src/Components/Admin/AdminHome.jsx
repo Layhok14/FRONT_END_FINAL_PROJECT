7866cf3248ef131bc9f3c97ee93354dda3572c75
Layhok14 <lenglayhok@gmail.com>
2026-03-16 21:08:33 +0700

    initailize

---
+++ src/Components/Admin/AdminHome.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+
+function AdminDashboard() {
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+      <p>This is the admin area.</p>
+    </div>
+  );
+}
+
+export default function AdminHome() {
+  return (
+    <div>
+      <h1>Admin Section</h1>
+      <nav>
+        <Link to="/admin">Dashboard</Link>
+        {/* add other admin links here */}
+      </nav>
+
+      <Routes>
+        <Route path="" element={<AdminDashboard />} />
+        {/* nested admin routes go here */}
+      </Routes>
+    </div>
+  );
+}
